3a9d86aebfc8c93f57760e2d6f46622a67ce416a
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2026-03-10 17:30:24 +0100

    fix.RoutineRepository.

--- ifit/src/main/java/com/uca/juangarcia/ifit/modules/training/repository/RoutineRepository.java
+++ ifit/src/main/java/com/uca/juangarcia/ifit/modules/training/repository/RoutineRepository.java
@@ -29,7 +29,7 @@ public interface RoutineRepository extends JpaRepository<Routine, Long> {
     /**
      * Encuentra todas las rutinas activas de un usuario.
      */
-    Routine findByUserIdAndIsActive(Long userId, boolean isActive);
+    List<Routine> findByUserIdAndIsActive(Long userId, boolean isActive);
     
     /**
      * Encuentra rutinas de un usuario con paginación.
